511f816ac3f31f8196450f863536a0a5e850418b
Björn Bergström <bjorn.bergstr@outlook.com>
2025-01-27 21:15:24 +0100

    create nextjs app

---
+++ nextjs-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="text-green-500">Hello, World!</h1>
+      </main>
+    </div>
+  );
+}
