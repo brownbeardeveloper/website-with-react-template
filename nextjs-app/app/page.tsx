7826e9758ff70ca1c997254a7b3ffdc2b0613f07
Björn Bergström <bjorn.bergstr@outlook.com>
2025-02-05 19:17:04 +0100

    remove nextjs app

--- nextjs-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="">
-      <main className="">
-        <h1 className="text-green-500">Hello, World!</h1>
-      </main>
-    </div>
-  );
-}
