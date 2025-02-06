9d94d0f8ff5f56aab03c4d976b63a1bb4af4ccd8
Björn Bergström <bjorn.bergstr@outlook.com>
2025-02-06 10:13:45 +0100

    navbar feature completed

--- vite-project/src/components/Header.tsx
+++ vite-project/src/components/Header.tsx
@@ -2,37 +2,44 @@ import SearchBar from "./SearchBar";
 import SkillSwipeIcon from "../assets/skillswipe.svg?react";
 
 export default function Header() {
-    return (
-        <header className="w-full fixed top-5 flex justify-center">
 
-            <nav className="p-4 flex items-center justify-center
+    const menuItems = [
+        { text: 'Home', href: '/' },
+        { text: 'About', href: '/about' },
+        { text: 'Services', href: '/services' },
+    ];
+
+    return (
+        <header className="w-full  fixed top-5 flex justify-center">
+            <nav className="w-9/10 max-w-200 flex items-center justify-between
                 bg-gradient-to-b from-slate-50 via-gray-100 to-slate-50
                 dark:bg-gradient-to-b dark:from-gray-800 dark:via-slate-900 dark:to-gray-800
-                rounded gap-10">
+                border-2 border-slate-100 dark:border-gray-900 shadow-lg rounded">
 
-                <a href="/" className="flex items-center no-wrap">
+                <a href="/" className="flex items-center -mt-1 ml-4 no-wrap">
                     <SkillSwipeIcon className="fill-yellow-600 dark:fill-gray-500" />
-                    <span className="ml-1 font-permanentmarker text-xl whitespace-nowrap text-gray-600 dark:text-white">
+                    <h1 className="ml-1 font-permanentmarker text-xl whitespace-nowrap text-gray-600 dark:text-white">
                         SkillSwipe
-                    </span>
+                    </h1>
                 </a>
 
-                <div className="flex md:order-2">
+                <div className="flex order-2 mr-4 py-2">
                     <SearchBar />
                 </div>
 
-                <div id="navbar-search" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium 
-                        md:space-x-8 md:flex-row md:mt-0 md:border-0">
-                        <li>
-                            <a href="#" className="transform transition-all duration-200 dark:text-white dark:hover:text-blue-500">Home</a>
-                        </li>
-                        <li>
-                            <a href="#" className="transform transition-all duration-200 dark:text-white dark:hover:text-blue-500">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="transform transition-all duration-200 dark:text-white dark:hover:text-blue-500">Services</a>
-                        </li>
+                <div id="navbar-search" className="hidden md:flex">
+                    <ul className="flex font-medium gap-3">
+                        {menuItems.map((item, index) => (
+                            <li key={index} className="relative group group-hover:cursor-pointer">
+                                <a href={item.href} className="block w-full h-full py-5 px-1.5 transform transition-all duration-200 
+                                    text-gray-800 dark:text-white
+                                    group-hover:text-orange-700 dark:group-hover:text-blue-400">
+                                    {item.text}
+                                </a>
+                                <div className="absolute bottom-3 left-0 w-full h-0.5 transition-all duration-200 rounded 
+                                    bg-orange-600 dark:bg-blue-500 scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"/>
+                            </li>
+                        ))}
                     </ul>
                 </div>
 
--- vite-project/src/components/SearchBar.tsx
+++ vite-project/src/components/SearchBar.tsx
@@ -8,8 +8,8 @@ export default function SearchBar() {
                     type="button"
                     aria-label="Search"
                     className="md:hidden p-2 border-2 border-gray-200 rounded-lg
-                    focus:outline-none focus:ring-4 focus:ring-blue-500 dark:focus:ring-pink-700
-                    cursor-pointer hover:ring-4 hover:ring-blue-200 dark:hover:ring-pink-400"
+                    focus:outline-none focus:ring-4 focus:ring-blue-500 dark:focus:ring-blue-700
+                    cursor-pointer hover:ring-3 hover:ring-blue-300 dark:hover:ring-blue-400"
                 >
                     <SearchIcon className="text-4xl text-slate-700 dark:text-slate-300" />
                 </button>
@@ -18,10 +18,13 @@ export default function SearchBar() {
                     <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none">
                         <SearchIcon className="text-3xl text-slate-700 dark:text-slate-300" />
                     </div>
-                    <input type="text" id="search-navbar" aria-label="Search" className="block w-full p-2 ps-10 text-sm 
-                    text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 
-                    dark:bg-gray-600 dark:border-gray-400 dark:placeholder-gray-300 dark:text-white
-                    dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                    <input type="text" id="search-navbar" placeholder="Search..." aria-label="Search"
+                        className="block w-full p-2 ps-10 text-sm border rounded-lg
+                        bg-gray-50 border-gray-300 placeholder-gray-500
+                        dark:bg-gray-600 dark:border-gray-400 dark:placeholder-gray-300
+                        hover:border-blue-500 hover:dark:border-blue-400
+                        focus:ring-blue-500 focus:border-blue-500 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
             </div >
         </>
